fc601eb709eaa11ecaf3769ce75f82a7baa7f64c
tribhu2001 <79187488+tribhu2001@users.noreply.github.com>
2023-10-10 16:33:51 +0530

    Update header.js

--- UI/src/components/header.js
+++ UI/src/components/header.js
@@ -5,9 +5,13 @@ function StickyHeader() {
   return (
     <>
       <Navbar style={{background: "linear-gradient(270deg,orange,lightblue,blue)", animation: "2s infinite alternate linear"}} sticky="top">
-          <div class="container" >
+          <div class="container" style={{justifyContent: "center"}}>
             <a ><img
               src={require('../Images/feedback.png')} width="150%" height="50" /></a>
+          </div>
+          <div class="container" style={{justifyContent: "center"}}>
+            <a ><img
+              src="https://cdn-icons-png.flaticon.com/128/893/893738.png" width="150%" height="50" /></a>
           </div>
       </Navbar>
       <br />
